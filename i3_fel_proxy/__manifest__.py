8e7a67552f0cf2b2266448bdbb3e22014b1fef80
gcarrillo <carrillo.g2015@gmail.com>
2025-02-06 16:53:25 -0600

    [FIX] taxes

--- i3_fel_proxy/__manifest__.py
+++ i3_fel_proxy/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'i3 FEL Guatemala',
-    'version': '16.0.0.0.2',
+    'version': '16.0.0.2.2',
     'category': 'Invoicing',
     'summary': 'Módulo Certificar en Guatemala-SAT',
     'description': """ 
